219b29be015563d0d8efe508a79c294c56d0c89e
SKXIAMO <1831630201@qq.com>
2024-12-24 18:23:12 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div class="your_style" rd-2 bg-black p-2 p-x-5 text-8 color-white>
         639293382@qq.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.your_style {
+  background-color: rgb(232, 119, 255);
+  color: black;
+}
+</style>
