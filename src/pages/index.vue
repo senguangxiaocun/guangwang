38f44d18e977da64e306bec92ad4e570fecfe132
SKXIAMO <1831630201@qq.com>
2025-01-02 10:12:56 +0800

    change

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div class="moverx-style" rd-2 bg-black p-2 p-x-5 text-8 color-white>
         639293382@qq.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.moverx-style {
+  background-color: #c5baf2;
+  color: black;
+}
+</style>
