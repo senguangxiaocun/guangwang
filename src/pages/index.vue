9d61477251f846f0332e976073d2ac247de8710b
SKXIAMO <1831630201@qq.com>
2025-01-08 17:12:45 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,15 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div class="heartache-email" rd-2 bg-black p-2 p-x-5 text-8 color-white>
         639293382@qq.com
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.heartache-email {
+  background-color: rgb(212, 169, 224);
+}
+</style>
