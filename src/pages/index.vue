58e21e881bab33ebb7c6384311e3bfbc0adb5989
SKXIAMO <1831630201@qq.com>
2025-05-07 15:26:55 +0800

    email

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div class="heartache-email" rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        Git783763@icloud.com
       </div>
     </div>
   </div>
