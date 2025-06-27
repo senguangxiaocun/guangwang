355ed035e58c8edf2bbbc96acc3b74e055058cea
SKXIAMO <1831630201@qq.com>
2025-06-27 10:29:44 +0800

    email

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        Lxh652837@icloud.com
+        fxm200621437@icloud.com
       </div>
     </div>
   </div>
