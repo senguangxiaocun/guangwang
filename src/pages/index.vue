c6613955eeb598bb7c6920d487ef9938a202cb50
SKXIAMO <1831630201@qq.com>
2026-06-16 18:32:54 +0800

    mail

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        zhangyuyang20070401@icloud.com
+        duongvandong7803@icloud.com
       </div>
     </div>
   </div>
